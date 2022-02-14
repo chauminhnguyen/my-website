5264ebf84b2d54bde8f2b9742920a7c812242105
minhchaunguyen <minhchauss123456@gmail.com>
2022-02-14 11:56:35 +0700

    first

---
+++ pages/home.js
@@ -0,0 +1,25 @@
+import Intro from '../components/Intro'
+import Info from '../components/Info'
+import Projects from '../components/Projects'
+import Head from 'next/head'
+
+export default function Home() {
+    return (
+        <html>
+            <Head>
+                <meta charSet="UTF-8" />
+                <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossOrigin="anonymous" />
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+                <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap" rel="stylesheet" />
+            </Head>
+            <body>
+                <Intro />
+                <Info />
+                <Projects />
+            </body>
+        </html>
+    )
+}
