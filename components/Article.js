70d4b3d4f77ee04c7413621d0f4b70f08d0bec06
chauminhnguyen <minhchauss123456@gmail.com>
2022-08-28 10:36:57 +0700

    add filter

--- components/Article.js
+++ components/Article.js
@@ -4,10 +4,12 @@ const Article = (props) => (
     <Link href={'/blogs/' + props.title}>
         <div className="article">
             <h2>{props.title}</h2>
-            <p>{props.time}</p>
-            <div className='categories'>
-                <p>{props.categories}</p>
-            </div>
+            <p className='time'>Date: {props.time}</p>
+            <Link href={'/blogs/' + props.categories}>
+                <div className='categories'>
+                    <p>{props.categories}</p>
+                </div>
+            </Link>
         </div>
     </Link>
 )
